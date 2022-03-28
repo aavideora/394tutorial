efbe2789b79ecc185e84e460ab54013ad574a896
aavi-deora <aavideora2022@u.northwestern.edu>
2022-03-28 13:56:46 -0500

    Style the app

--- src/App.js
+++ src/App.js
@@ -31,11 +31,10 @@ const Banner = props => (
 )
 
 const CourseList = ({ courses }) => (
-    <div>
+    <div className="course-list">
         {Object.values(courses).map(course => <Course key={course.id} course={course} />)}
     </div>
 );
-
 const terms = { F: 'Fall', W: 'Winter', S: 'Spring'};
 
 const getCourseTerm = course => (
@@ -47,12 +46,16 @@ const getCourseNumber = course => (
 );
 
 const Course = ({ course }) => (
-  <div>
-    { getCourseTerm(course) } CS { getCourseNumber(course) }: { course.title }
-  </div>
+    <div className="card m-1 p-2">
+        <div className="card-body">
+            <div className="card-title">{getCourseTerm(course)} CS {getCourseNumber(course)}</div>
+            <div className="card-text">{course.title}</div>
+        </div>
+    </div>
 );
+
 const App = () => (
-    <div>
+    <div className="container">
         <Banner title={schedule.title} />
         <CourseList courses={schedule.courses} />
     </div>
