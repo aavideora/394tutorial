9889a84235a17068a0d11813534705d1eafc779b
aavi-deora <aavideora2022@u.northwestern.edu>
2022-03-28 16:07:23 -0500

    mod

---
+++ src/components/CourseList.js
@@ -0,0 +1,59 @@
+﻿import React, { useState, useEffect } from 'react';
+import { hasConflict, terms, days, courseConflict, getCourseTerm, timeConflict } from '../utilities/times.js';
+import Course from './Course';
+
+const CourseList = ({ courses }) => {
+    const [term, setTerm] = useState('Fall');
+    const [selected, setSelected] = useState([]);
+    const termCourses = Object.values(courses).filter(course => term === getCourseTerm(course));
+
+    return (
+        <>
+            <TermSelector term={term} setTerm={setTerm} />
+            <div className="course-list">
+                {
+                    termCourses.map(course =>
+                        <Course key={course.id} course={course}
+                            selected={selected} setSelected={setSelected}
+                        />)
+                }
+            </div>
+        </>
+    );
+};
+
+
+const TermSelector = ({ term, setTerm }) => (
+    <div className="btn-group">
+        {
+            Object.values(terms).map(value => (
+                <TermButton key={value} term={value} setTerm={setTerm} checked={value === term} />
+            ))
+        }
+    </div>
+);
+
+
+
+const getCourseNumber = course => (
+  course.id.slice(1, 4)
+);
+
+
+const toggle = (x, lst) => (
+    lst.includes(x) ? lst.filter(y => y !== x) : [x, ...lst]
+);
+
+const TermButton = ({ term, setTerm, checked }) => (
+    <>
+        <input type="radio" id={term} className="btn-check" checked={checked} autoComplete="off"
+            onChange={() => setTerm(term)} />
+        <label class="btn btn-success m-1 p-2" htmlFor={term}>
+            {term}
+        </label>
+    </>
+);
+
+export default CourseList;
+
+
---
+++ src/times.js
@@ -0,0 +1,18 @@
+﻿export const hasConflict = (course, selected) => (
+    selected.some(selection => courseConflict(course, selection))
+);
+export const terms = { F: 'Fall', W: 'Winter', S: 'Spring' };
+
+export const days = ['M', 'Tu', 'W', 'Th', 'F'];
+export const courseConflict = (course1, course2) => (
+    getCourseTerm(course1) === getCourseTerm(course2)
+    && timeConflict(course1, course2)
+);
+
+export const getCourseTerm = course => (
+    terms[course.id.charAt(0)]
+);
+export const timeConflict = (course1, course2) => (
+    daysOverlap(course1.days, course2.days) && hoursOverlap(course1.hours, course2.hours)
+);
+
